dff078411d05e47138888824598ce5d3efa39732
yoora <espoir3045@gmail.com>
2024-12-02 10:39:34 +0900

    초기이미지 설정

--- rockpaperscissor_game/src/components/Box.js
+++ rockpaperscissor_game/src/components/Box.js
@@ -4,7 +4,7 @@ const Box = (props) =>{
   return (
     <div className={"game_box is_" + props.result}>
       <strong className="game_user">{props.user}</strong>
-      <img src={props.item && props.item.ob} alt="이미지 설명"/>
+      <img src={!props.result? 'Rock' : props.item.ob} alt="이미지"/>
       <strong className="game_result">{props.result}</strong>
     </div>
   )
